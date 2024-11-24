fc5206f4e1b9f05d54f6034b0d12466156da5daf
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-25 00:53:14 +0200

    backend: add a login function and a function to respond to jwtToken

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -34,5 +34,27 @@ class AuthController extends Controller
     ]);
 }
 
-    
+    // Login user
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (! $token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+
+
+    // Return JWT token response
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => JWTAuth::factory()->getTTL() * 60
+        ]);
+    }
 }
