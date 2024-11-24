3ca38a0bed347a3ecc55d9f3dedca8bf5dec5e90
Maya Eid <mayabaseleid@gmail.com>
2024-11-24 23:57:55 +0200

    frontend: Create an Output component and add it to the CodeEditor component

--- frontend/src/pages/CodeEditor.jsx
+++ frontend/src/pages/CodeEditor.jsx
@@ -1,24 +1,30 @@
-import { Box } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
 import { Editor } from "@monaco-editor/react";
 import { useContext } from "react";
 import { codeContext } from "../context/codeContext";
 import { codeSnippets } from "../utils/enums/constants";
 import LanguageSelector from "../components/base/LanguageSelector";
+import Output from "./Output.jsx";
 
 const CodeEditor = () => {
-  const { value, setValue, selectedLanguage, onSelect } = useContext(codeContext);
+  const { value, setValue, selectedLanguage } =
+    useContext(codeContext);
+
   return (
-    <Box w="50%">
-      <LanguageSelector />
-      <Editor
-        height="75vh"
-        language={selectedLanguage}
-        defaultValue={codeSnippets[selectedLanguage]}
-        theme="vs-dark"
-        value={value}
-        onChange={value => setValue(value)}
-      />
-    </Box>
+    <HStack spacing={4}>
+      <Box w="50%">
+        <LanguageSelector />
+        <Editor
+          height="75vh"
+          language={selectedLanguage}
+          defaultValue={codeSnippets[selectedLanguage]}
+          theme="vs-dark"
+          value={value}
+          onChange={(value) => setValue(value)}
+        />
+      </Box>
+      <Output />
+    </HStack>
   );
 };
 
