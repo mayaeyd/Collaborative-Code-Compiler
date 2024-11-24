59a4aa8fb027fee47e27d63ea316a917dc67e37d
Abdul Rahmna Al Saadi <abedsaadi987@gmail.com>
2024-11-24 21:48:39 +0200

    Backend: created files table migration

---
+++ backend/database/migrations/2024_11_24_193644_create_files_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->tinyText('name');
+            $table->text('content');
+            $table->enum('language', ['javascript', 'python', 'csharp'])->default('javascript');
+            $table->unsignedBigInteger('owner_id');
+
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
