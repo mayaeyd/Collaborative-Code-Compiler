1bdd2c9bd44cfe42af0f8b8736e6eabf800f646a
Maya Eid <mayabaseleid@gmail.com>
2024-11-25 00:31:48 +0200

    backend: Validate the data sent to the API responsible to run the code

--- backend/app/Http/Controllers/OutputController.php
+++ backend/app/Http/Controllers/OutputController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class OutputController extends Controller
 {
     function run_code(Request $request){
-        
+        $validated = $request->validate([
+            'language' => 'required|string',
+            'verison'=> 'required|string',
+            'content'=> 'required|string',
+        ]);
+
     }
 }
