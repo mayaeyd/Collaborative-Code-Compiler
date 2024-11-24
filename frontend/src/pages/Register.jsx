7b7d5c6ab9caea0c7c0346ec1ba6ba0c7ec01fda
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-24 22:47:00 +0200

    frontend:add the input styling in the Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, theme, Container } from '@chakra-ui/react';
+import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, theme, Container, Input, StackDivider } from '@chakra-ui/react';
 
 const Register = () => {
   return (
     <ChakraProvider theme={theme}>
       <Container>
-        <Box>
-          
+        <Box borderWidth="1px" borderRadius="1g" p={4}>
+          <Text fontSize="2xl" align="center">Create Account</Text>
+          <Text fontSize="md" align="center" pb={4}>Already have an account? <Link color="teal.500" href='#'>Log in</Link></Text>
+          <VStack
+            spacing={2}
+            align="stretch"
+          >
+            <Input placeholder='Username' />
+            <Input placeholder='Email' />
+            <Input placeholder='Password' />
+            <Input placeholder='Confirm Password' />
+          </VStack>
+
+
         </Box>
       </Container>
     </ChakraProvider>
