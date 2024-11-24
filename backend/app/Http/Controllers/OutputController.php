d8293b4c933e84aec92a035380a7a9616bc62ef0
Maya Eid <mayabaseleid@gmail.com>
2024-11-25 00:28:53 +0200

    backend: Create an output controller to store the run code api method and create its route

---
+++ backend/app/Http/Controllers/OutputController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OutputController extends Controller
+{
+    function run_code(Request $request){
+        
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\OutputController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/compiler',[OutputController::class,'run_code']);
