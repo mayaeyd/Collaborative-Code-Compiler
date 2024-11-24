6d60b57539e822406739e87da9853aeea88558d2
Maya Eid <mayabaseleid@gmail.com>
2024-11-24 21:22:58 +0200

    Create CodeEditor component with minimal styles

---
+++ frontend/src/pages/CodeEditor.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const CodeEditor = () => {
+  return (
+    <Box w='50%'>
+      Hello
+    </Box>
+  )
+}
+
+export default CodeEditor
--- frontend/src/pages/Compiler.jsx
+++ frontend/src/pages/Compiler.jsx
@@ -1,5 +1,5 @@
 import { Box } from "@chakra-ui/react";
-import CodeEditor from "./CodeEditor";
+import CodeEditor from "./CodeEditor.jsx";
 
 function Compiler() {
   return (
