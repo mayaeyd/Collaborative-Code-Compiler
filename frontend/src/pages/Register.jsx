f4511989fb7d3c99669407216a67e649638bcbf5
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-24 22:58:20 +0200

    frontend:add the Buttons and allign the container to the center

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
-import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, theme, Container, Input, StackDivider } from '@chakra-ui/react';
+import { ChakraProvider, Box, Text, Link, VStack, Input, Button, theme, Container } from '@chakra-ui/react';
 
 const Register = () => {
   return (
     <ChakraProvider theme={theme}>
-      <Container>
-        <Box borderWidth="1px" borderRadius="1g" p={4}>
-          <Text fontSize="2xl" align="center">Create Account</Text>
-          <Text fontSize="md" align="center" pb={4}>Already have an account? <Link color="teal.500" href='#'>Log in</Link></Text>
-          <VStack
-            spacing={2}
-            align="stretch"
-          >
-            <Input placeholder='Username' />
-            <Input placeholder='Email' />
-            <Input placeholder='Password' />
-            <Input placeholder='Confirm Password' />
+      <Container
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <Box borderWidth="1px" borderRadius="lg" p={6} maxWidth="400px" width="100%">
+          <Text fontSize="2xl" align="center" mb={4}>
+            Create Account
+          </Text>
+          <Text fontSize="md" align="center" mb={6}>
+            Already have an account?{' '}
+            <Link color="teal.500" href="#">
+              Log in
+            </Link>
+          </Text>
+          <VStack spacing={4} align="stretch" mb={6}>
+            <Input placeholder="Username" />
+            <Input placeholder="Email" />
+            <Input placeholder="Password" type="password" />
+            <Input placeholder="Confirm Password" type="password" />
           </VStack>
-
-
+          <Button colorScheme="teal" variant="solid" width="100%">
+            Create Account
+          </Button>
         </Box>
       </Container>
     </ChakraProvider>
   );
-}
+};
 
 export default Register;
