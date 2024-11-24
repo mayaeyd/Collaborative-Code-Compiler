babd0aeec72c843e85f16c92d5656acf07fece36
Maya Eid <mayabaseleid@gmail.com>
2024-11-25 01:12:47 +0200

    backend: Fetch the API response after sending it code info

--- frontend/src/pages/Output.jsx
+++ frontend/src/pages/Output.jsx
@@ -4,9 +4,26 @@ import { Box, Button, Text } from "@chakra-ui/react";
 import { LANGUAGE_VERSIONS } from "../utils/enums/constants";
 
 const Output = () => {
-    const {value, setValue, selectedLanguage, onSelect, output, setOutput} = useContext(codeContext);
+    const {value, selectedLanguage, output, setOutput} = useContext(codeContext);
     const version = LANGUAGE_VERSIONS[selectedLanguage];
     
+    const runCode = async () => {
+      try {
+          setOutput('Loading...');
+        const response = await axios.post(
+          "http://127.0.0.1:8000/api/compiler",
+          {
+            selectedLanguage,
+            version,
+            value,
+          }
+        );
+        setOutput(response.data.run.output);
+      } catch (error) {
+          console.error('Error running code:', error);
+      }
+    };
+
   return (
     <Box w="50%">
       <Text mb={2} fontSize="lg">
