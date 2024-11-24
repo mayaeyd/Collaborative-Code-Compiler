81de20d2d4050c31a22f33a6688a5a29d1cd0e1f
Maya Eid <mayabaseleid@gmail.com>
2024-11-24 23:02:15 +0200

    frontend: Use codeContext in CodeEditor component

--- frontend/src/pages/CodeEditor.jsx
+++ frontend/src/pages/CodeEditor.jsx
@@ -1,14 +1,19 @@
 import { Box } from "@chakra-ui/react";
 import { Editor } from "@monaco-editor/react";
+import { useContext } from "react";
+import { codeContext } from "../context/codeContext";
 
 const CodeEditor = () => {
+  const { value, setValue, selectedLanguage, onSelect } = useContext(codeContext);
   return (
     <Box w="50%">
       <Editor
         height="75vh"
-        language="javascript"
-        defaultValue="// some comment"
+        language={selectedLanguage}
+        defaultValue={codeSnippets[selectedLanguage]}
         theme="vs-dark"
+        value={value}
+        onChange={value => setValue(value)}
       />
     </Box>
   );
