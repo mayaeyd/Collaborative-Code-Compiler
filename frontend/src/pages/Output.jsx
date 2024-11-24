c791b60f82adf06bb4700237b6c7fa64ac2b5f7a
Maya Eid <mayabaseleid@gmail.com>
2024-11-25 00:03:10 +0200

    frontend: Implement the output container and apply stylings

--- frontend/src/pages/Output.jsx
+++ frontend/src/pages/Output.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useContext } from "react"
+import { codeContext } from "../context/codeContext"
+import { Box, Button, Text } from "@chakra-ui/react";
 
 const Output = () => {
+    const {value, setValue, selectedLanguage, onSelect} = useContext(codeContext);
   return (
-    <div>
-      
-    </div>
-  )
+    <Box w="50%">
+      <Text mb={2} fontSize="lg">
+        Output
+      </Text>
+      <Button
+        variant="outline"
+        colorScheme="green"
+        mb={4}
+      >
+        Run Code
+      </Button>
+      <Box
+        height="75vh"
+        p={2}
+        border="1px solid"
+        borderRadius={4}
+        borderColor="#333"
+      >
+        Hello
+      </Box>
+    </Box>
+  );
 }
 
 export default Output
