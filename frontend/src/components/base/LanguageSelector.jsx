ba46ad8c0ebae818fbd0bdfe83ff3a638df34824
Maya Eid <mayabaseleid@gmail.com>
2024-11-24 23:50:24 +0200

    frontend: Render the selected language in the dropdown menu and add a code snippet for each language

--- frontend/src/components/base/LanguageSelector.jsx
+++ frontend/src/components/base/LanguageSelector.jsx
@@ -8,21 +8,35 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { LANGUAGE_VERSIONS } from "../../utils/enums/constants";
+import { useContext } from "react";
+import { codeContext } from "../../context/codeContext";
 
 const languages = Object.entries(LANGUAGE_VERSIONS);
+const activeColor = "blue.400";
 
 const LanguageSelector = () => {
+  const { selectedLanguage, onSelect } = useContext(codeContext);
+
   return (
-    <Box>
-      <Text ml={2} mb={4}>
+    <Box ml={2} mb={4}>
+      <Text mb={2} fontSize="lg">
         Language:
       </Text>
-      <Menu>
-        <MenuButton as={Button}>javascript</MenuButton>
-        <MenuList bg="#110c1b">
-            <MenuItem>javascript</MenuItem>
-            <MenuItem>python</MenuItem>
-            <MenuItem>csharp</MenuItem>
+      <Menu isLazy>
+        <MenuButton as={Button}>{selectedLanguage}</MenuButton>
+        <MenuList>
+          {languages.map(([language, version]) => (
+            <MenuItem
+              key={language}
+              onClick={() => onSelect(language)}
+            >
+              {language}
+              &nbsp;&nbsp;&nbsp;
+              <Text as="span" fontSize={"sm"}>
+                ({version})
+              </Text>
+            </MenuItem>
+          ))}
         </MenuList>
       </Menu>
     </Box>
