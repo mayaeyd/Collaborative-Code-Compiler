7472ac21de4f0bc58c5c2d14141d8ac711a86850
Maya Eid <mayabaseleid@gmail.com>
2024-11-25 10:25:06 +0200

    backend: create a route to send a test email

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmailController;
+use App\Mail\RequestCollab;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/send-test-email', function () {
+    Mail::to('mayabaseleid@gmail.com')->send(new RequestCollab());
+    return 'Test email sent!';
 });
