e67eb65c3c33f4a7b41ce7b3d792379ab633f29d
Maya Eid <mayabaseleid@gmail.com>
2024-11-25 16:37:23 +0200

    backend: recreate the route to the output controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,9 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RequestCollabController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+Route::post('/compiler', [OutputController::class, 'run_code']);
 Route::post('/send-test-email', [RequestCollabController::class, 'sendTestEmail']);
 
