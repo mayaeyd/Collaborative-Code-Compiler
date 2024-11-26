f4b413e2a1a0a12935f6dd1377d6fc8e550dd7bb
Maya Eid <mayabaseleid@gmail.com>
2024-11-26 06:30:59 +0200

    frontend: wrap App with email context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 import CodeProvider from "./context/codeContext";
+import EmailProvider from "./context/emailContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider theme={theme}>
     <CodeProvider>
-      <App />
+      <EmailProvider>
+        <App />
+      </EmailProvider>
     </CodeProvider>
   </ChakraProvider>
 );
-
