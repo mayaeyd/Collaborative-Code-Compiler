99a1de909b54b6cba04985f39a0832b237853b50
Maya Eid <mayabaseleid@gmail.com>
2024-11-27 16:31:33 +0200

    frontend: style AIPopup component

--- frontend/src/components/common/AIPopup.jsx
+++ frontend/src/components/common/AIPopup.jsx
@@ -2,6 +2,16 @@ import React, { useEffect } from "react";
 import { Box, Text, Button, background } from "@chakra-ui/react";
 
 const AIPopup = ({ line, suggestion, onClose }) => {
+    
+  const popupStyle = {
+    position: "absolute",
+    top: `${line * 26}px`,
+    left: "40%",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+    padding: "20px",
+    zIndex: 1000,
+  };
 
   return (
     <Box style={popupStyle} backgroundColor="green.200">
