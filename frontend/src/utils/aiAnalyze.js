eab964add30260f97377f06fdbd5a6ce00a9a8c1
Maya Eid <mayabaseleid@gmail.com>
2024-11-27 13:08:41 +0200

    frontend: return response of ai from aiAnalyze external function

--- frontend/src/utils/aiAnalyze.js
+++ frontend/src/utils/aiAnalyze.js
@@ -1,3 +1,13 @@
-export const aiAnalyze = (output) => {
-  console.log("in ai function ", output);
+import axios from "axios";
+
+export const aiAnalyze = async (output) => {
+    try{
+        const response = await axios.post('http://127.0.0.1:8000/api/analyze-code',{
+            error:output
+        });
+        return response.data.choices[0].message.content;
+        
+    }catch(error){
+        console.error(error);
+    }
 };
