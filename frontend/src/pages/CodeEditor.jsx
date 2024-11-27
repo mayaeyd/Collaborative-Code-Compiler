3079f526a610a2a1957bff535966b693f60c7b9b
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-27 03:31:16 +0200

    frontend: created a save button and make its handleSave function

--- frontend/src/pages/CodeEditor.jsx
+++ frontend/src/pages/CodeEditor.jsx
@@ -7,26 +7,33 @@ import LanguageSelector from "../components/base/LanguageSelector";
 import Output from "./Output.jsx";
 
 const CodeEditor = () => {
-  const { value, setValue, selectedLanguage } =
-    useContext(codeContext);
+  const { value, setValue, selectedLanguage } = useContext(codeContext);
+
+  const handleSave = () => {
+    alert("Code saved!");
+  };
 
   return (
     <>
-    <HStack spacing={4}>
-    {/* files scroller  */}
-      <Box w="50%">
-        <LanguageSelector />
-        <Editor
-          height="75vh"
-          language={selectedLanguage}
-          defaultValue={codeSnippets[selectedLanguage]}
-          theme="vs-dark"
-          value={value}
-          onChange={(value) => setValue(value)}
-        />
-      </Box>
-      <Output />
-=    </HStack>
+      <HStack spacing={4}>
+        <Box w="50%">
+          <HStack spacing={4} align="center">
+            <LanguageSelector />
+            <Button colorScheme="blue" onClick={handleSave} mt={5}>
+              Save
+            </Button>
+          </HStack>
+          <Editor
+            height="75vh"
+            language={selectedLanguage}
+            defaultValue={codeSnippets[selectedLanguage]}
+            theme="vs-dark"
+            value={value}
+            onChange={(value) => setValue(value)}
+          />
+        </Box>
+        <Output />
+      </HStack>
     </>
   );
 };
--- frontend/src/pages/Compiler.jsx
+++ frontend/src/pages/Compiler.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../context/AuthContext.jsx";
 import CodeEditor from "./CodeEditor.jsx";
 
 function Compiler() {
-  const {user, logout} = useAuth();
+  const { user, logout } = useAuth();
 
   const handleLogout = () => {
     logout();
@@ -13,7 +13,7 @@ function Compiler() {
     <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
       <HStack spacing={4} mb={4} align="center">
         <Text fontSize="lg" fontWeight="bold">
-          Welcome, John Doe!
+          Welcome, {user ? user.name : "Guest"}!
         </Text>
         <Button colorScheme="red" onClick={handleLogout}>
           Logout
