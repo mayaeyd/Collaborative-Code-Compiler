0bd04d6115f33b4e39a2c6777619ded27d24e1e7
Maya Eid <mayabaseleid@gmail.com>
2024-11-27 16:49:46 +0200

    frontend: fix popup styling

--- frontend/src/components/common/AIPopup.jsx
+++ frontend/src/components/common/AIPopup.jsx
@@ -2,22 +2,24 @@ import React, { useEffect } from "react";
 import { Box, Text, Button, background } from "@chakra-ui/react";
 
 const AIPopup = ({ line, suggestion, onClose }) => {
-    
+
   const popupStyle = {
     position: "absolute",
-    top: `${line * 26}px`,
-    left: "40%",
+    top: `35%`,
+    left: "35%",
     border: "1px solid #ccc",
     borderRadius: "5px",
     padding: "20px",
     zIndex: 1000,
+    maxWidth:'400px'
   };
 
   return (
     <Box style={popupStyle} backgroundColor="green.200">
       <Text fontWeight="bold">AI Suggestion:</Text>
+      <Text>On line {line}</Text>
       <Text>{suggestion}</Text>
-      <Button mt={2} colorScheme="blue" size="sm" onClick={onClose}>
+      <Button mt={2} size="sm" colorScheme="blue" onClick={onClose}>
         Close
       </Button>
     </Box>
