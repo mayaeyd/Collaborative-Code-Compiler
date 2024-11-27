54c89449f9932c78575654c990ce0320705ba43d
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-27 23:21:06 +0200

    frontend: add the try catch to fetch the api

--- frontend/src/context/saveContext.js
+++ frontend/src/context/saveContext.js
@@ -7,7 +7,28 @@ export const saveContext = createContext();
 const SaveProvider = ({ children }) => {
   const { value, selectedLanguage } = useContext(codeContext);  
 
-  
+  const handleSave = async (fileName) => {
+    try {
+      console.log('Saving file with the following details:');
+      console.log('Name:', fileName);
+      console.log('Content:', value);  
+      console.log('Language:', selectedLanguage);  
+
+      const token = localStorage.getItem('token');  
+      if (!token) {
+        console.log('No token found. Please log in again.');
+        alert('You need to be logged in to save files.');
+        return;  
+      }
+
+      
+  };
+
+  return (
+    <saveContext.Provider value={{ handleSave }}>
+      {children}
+    </saveContext.Provider>
+  );
 };
 
 export default SaveProvider;
