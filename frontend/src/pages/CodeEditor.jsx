06bc3929f3bfa6b2ee735ad00b0961f89ae618b3
Abdul Rahmna Al Saadi <abedsaadi987@gmail.com>
2024-11-27 13:16:12 +0200

    frontend: add litener to events coming from server

--- frontend/src/pages/CodeEditor.jsx
+++ frontend/src/pages/CodeEditor.jsx
@@ -6,12 +6,63 @@ import { codeContext } from "../context/codeContext";
 import { codeSnippets } from "../utils/enums/constants";
 import LanguageSelector from "../components/base/LanguageSelector";
 import Output from "./Output.jsx";
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+import { useDebouncedCallback } from "use-debounce";
 
 const CodeEditor = () => {
+  const [text, setText] = useState('');
+  const [isTyping, setIsTyping] = useState(false);
   const { value, setValue, selectedLanguage } =
     useContext(codeContext);
 
+    window.Pusher = Pusher;
 
+    useEffect(() => {
+      const echo = new Echo({
+        broadcaster: 'reverb',
+        key: 'fgzztbpaq3qu9r6ibtco', 
+        host: "localhost",
+        wsHost: "localhost",
+        wsPort: 8080,  
+        forceTLS: false, 
+        disableStats: true, 
+        enabledTransports: ['ws', 'wss'], 
+      });
+  
+      console.log(echo);
+  
+      echo.channel('text-updates').listen('TextChangedEvent', (event) => {
+        if (!isTyping && event.text !== text) {
+          // setIncomingText(event.text);
+          console.log("Received: ", event.text);
+          setValue(event.text); 
+        }
+      });
+  
+      return () => {
+        echo.disconnect();
+      };
+    }, []); 
+
+    const postRequest = useCallback(() => {
+      fetch('http://127.0.0.1:8000/api/update-text', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text }),
+      })
+    }, [text]);
+
+    const debouncedPostRequest = useDebouncedCallback(postRequest, 500);
+
+    const handleInputChange = (newValue) => {
+      setText(newValue);
+      debouncedPostRequest(); 
+      setIsTyping(true);
+      setTimeout(() => setIsTyping(false), 500);
+    };
 
   return (
     <>
